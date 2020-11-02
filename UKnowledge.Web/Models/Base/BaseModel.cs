b87a26ae1db950ee11fa14b72e8dfa0901da8981
sksami3 <sksami4456@gmail.com>
2020-11-02 22:18:57 +0600

    Add project files.

---
+++ UKnowledge.Web/Models/Base/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UKnowledge.Web.Models.Base
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public double Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
